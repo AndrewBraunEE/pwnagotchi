5c7a0e1b00750e0d87fb4e56448b806858df440a
Andrew <atb197@g.ucla.edu>
2023-12-22 17:27:59 -0800

    Add stop update

--- scripts/stop_pwnagotchi.sh
+++ scripts/stop_pwnagotchi.sh
@@ -1,3 +1,5 @@
 BACKUP_FILE="127.0.0.1-backup-1703029168.tgz"
+sudo systemctl disable pwnagotchi_browser;
+sudo systemctl disable start_pwnagotchi_automatically;
 sudo ./restore.sh -b $BACKUP_FILE;
 sudo ifconfig wlan0 down && sudo ifconfig mon0 down && sudo iwconfig mon0 mode Managed && sudo iwconfig wlan0 mode Managed && sudo service network-manager restart && sudo ifconfig wlan0 up && sudo ifconfig mon0 up
